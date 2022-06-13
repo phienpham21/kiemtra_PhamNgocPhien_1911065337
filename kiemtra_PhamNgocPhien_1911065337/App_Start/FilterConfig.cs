f2ef164c20b8cd27664bc9180153926140a266ce
Admin <Admin@LAPTOP-CP9KINJA>
2022-06-13 13:13:10 +0700

    Add project files.

---
+++ kiemtra_PhamNgocPhien_1911065337/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace kiemtra_PhamNgocPhien_1911065337
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ kiemtra_PhamNgocPhien_1911065337/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kiemtra_PhamNgocPhien_1911065337.MvcApplication" Language="C#" %>
